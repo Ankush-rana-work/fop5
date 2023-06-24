0edf09cc60733c90e04529f246a813cb8e0f6041
Ankush Rana <ankush.rana@techahead.com>
2023-06-24 18:15:58 +0530

    initial comment

---
+++ assets/js/custom.js
@@ -0,0 +1,81 @@
+$('.slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:false,
+    autoplay: true,
+    slideTransition: 'linear',
+    autoplayTimeout: 6000,
+    autoplaySpeed: 700,
+    smartSpeed: 700,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.fallenHeroes-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    dots:true,
+    autoplay: true,
+    slideTransition: 'linear',
+    autoplayTimeout: 6000,
+    autoplaySpeed: 700,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        767:{
+            items:3
+        },
+        991:{
+            items:5
+        }
+
+    }
+})
+$('.fadeSlider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots:false,
+    autoplay: true,
+    slideTransition: 'linear',
+    autoplayTimeout: 6000,
+    autoplaySpeed: 700,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        767:{
+            items:1
+        },
+        991:{
+            items:1
+        }
+
+    }
+});
+
+
+$(document).ready(function(){
+    $('.btnToggle').click(function(){
+        $('header .mainNavbar .menubarWrap ul').stop().slideToggle();
+    });
+});
+
+new WOW().init();
